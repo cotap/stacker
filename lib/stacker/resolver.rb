2f9b343c38b045cabc52933d0d68fe7ae579426c
James Hamlin <jfhamlin@cotap.com>
2016-04-30 15:31:59 -0700

    Add support for array parameters in region files.
    Consider a stack A that accepts a `CommaDelimitedList` parameter. Say we have
    multiple stacks; B, C, and D; whose outputs we wish to join in a list to
    provide to A as this parameter. Previously, there was no way to do this; stack
    outputs could only be used as simple parameters. This change allows YAML
    arrays to be supplied as parameter values in region files. At resolution time,
    the array values are concatenated to form a comma-delimited list. Array
    elements that are dictionaries are treated as stack output references, and
    resolved.
    
    Signed-off-by: James Hamlin <jfhamlin@cotap.com>

--- lib/stacker/resolver.rb
+++ lib/stacker/resolver.rb
@@ -8,21 +8,31 @@ module Stacker
     end
 
     def dependencies
-      @dependencies ||= parameters.select { |_, value|
+      @dependencies ||= parameters.values.flatten.select { |value|
         value.is_a?(Hash)
-      }.map { |_, value|
-        "#{value.fetch('Stack')}.#{value.fetch('Output')}"
+      }.map { |ref|
+        "#{ref.fetch('Stack')}.#{ref.fetch('Output')}"
       }
     end
 
     def resolved
       @resolved ||= Hash[parameters.map do |name, value|
         if value.is_a? Hash
-          stack = region.stack value.fetch('Stack')
-          value = stack.outputs.fetch value.fetch('Output')
+          value = resolve_reference(value)
+        elsif value.is_a? Array
+          value = value.map do |ref|
+            ref.is_a?(Hash) ? resolve_reference(ref) : ref
+          end.join ','
         end
         [ name, value ]
       end]
+    end
+
+    private
+
+    def resolve_reference(ref)
+      stack = region.stack ref.fetch('Stack')
+      stack.outputs.fetch ref.fetch('Output')
     end
 
   end
